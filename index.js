3a7f42c2865d6c1590a7459e3fbdd3652688eb2c
Timo Jarventausta <timo.jaerv@gmail.com>
2016-12-25 22:43:17 +0200

    Add single endpoint JSON API

---
+++ index.js
@@ -0,0 +1,23 @@
+/* jshint node: true, esversion: 6 */
+'use strict';
+
+var express = require("express"),
+		bodyParser = require("body-parser"),
+		Invoice = require("./models/invoice");
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+	var invoice = new Invoice(req.body.invoice);
+	invoice.toBuffer().then((buf) => {
+		res.type('pdf').send(buf);
+	}, (err) => {
+		res.send(err);
+	});
+});
+
+app.listen(process.env.PORT, process.env.IP, () => {
+	console.log("Genvoice server is running!");
+});
---
+++ models/invoice.js
@@ -0,0 +1,58 @@
+/* jshint node: true, esversion: 6 */
+'use strict';
+
+var moment = require("moment"),
+		pug = require("pug"),
+		pdf = require("html-pdf"),
+		moment = require("moment"),
+		helpers = require("../helpers");
+
+module.exports = function(invoice) {
+
+	//Calculate totals
+	invoice.total = 0;
+	invoice.items.forEach((item) => {
+		item.tax = item.tax || 0;
+		item.subtotal = (item.price * item.amount) * (1 + item.tax);
+		invoice.total += item.subtotal;
+		item.unit = item.unit || "-";
+	});
+
+	//Set dates
+	invoice.date = {};
+	invoice.date.created = moment().format("D.M.Y");
+	invoice.date.due = moment().add(invoice.term, "days").format("D.M.Y");
+
+	//Generate reference
+	var id = ("" + moment().valueOf()).substr(-5);
+	invoice.reference = helpers.generateReference(id);
+
+	var html = pug.renderFile('./templates/invoice.pug', {invoice: invoice});
+	var filename = "invoice_" + invoice.reference + ".pdf";
+	var options = {
+		format: "A4",
+		border: '10mm',
+		quality: "80"
+	};
+
+	this.save = () => new Promise((resolve, reject) => {
+			pdf.create(html, options).toFile("./public/" + filename, (err) => {
+				if(err) {
+					reject(err);
+				} else {
+					resolve(filename);
+				}
+			});
+	});
+
+	this.toBuffer = () => new Promise((resolve, reject) => {
+			pdf.create(html, options).toBuffer((err, buf) => {
+				if(err) {
+					reject(err);
+				} else {
+					resolve(buf);
+				}
+			});
+	});
+
+};
